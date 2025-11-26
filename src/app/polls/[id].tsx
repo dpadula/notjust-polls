65ee8f004a7efbc99dc115c163b640548676d2ac
Diego <dpadula@epe.santafe.gov.ar>
2025-11-26 12:27:21 -0300

    Agregando detalles a cada poll

--- src/app/polls/[id].tsx
+++ src/app/polls/[id].tsx
@@ -2,11 +2,21 @@ import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const poll = {
+  question: 'What is your favorite color?',
+  options: ['Red', 'Green', 'Blue'],
+};
+
 const PollDetails = () => {
   const { id } = useLocalSearchParams();
   return (
     <View>
-      <Text>PollDetails {id}</Text>
+      <Text>
+        {poll.question} {id}
+      </Text>
+      {poll.options.map((option) => (
+        <Text key={option}>{option}</Text>
+      ))}
     </View>
   );
 };
