9e638b6a2b33cb36289a53e6b462373ef7175c51
Diego <dpadula@epe.santafe.gov.ar>
2025-11-26 13:56:51 -0300

    mejorado de encabezado

--- src/app/polls/new.tsx
+++ src/app/polls/new.tsx
@@ -1,9 +1,23 @@
+import { Stack } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const CreatePoll = () => {
   return (
     <View>
+      <Stack.Screen
+        options={{
+          title: 'Polls',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => <Text>Save</Text>,
+        }}
+      />
       <Text>CreatePoll</Text>
     </View>
   );
