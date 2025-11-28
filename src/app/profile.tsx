3738ced76392d384db88b739e66cf11a404b838f
Diego <dpadula@epe.santafe.gov.ar>
2025-11-28 14:03:18 -0300

    Si el usuario no esta logueado, lo redirige a login en el profile

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,11 +1,13 @@
+import { Redirect } from 'expo-router';
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { supabase } from '../lib/supabase';
 import { useAuth } from './providers/AuthProvider';
 
 const ProfileScreen = () => {
-  const { session } = useAuth();
+  const { session, user } = useAuth();
 
+  if (!user) return <Redirect href='/login' />;
   return (
     <View>
       {session && session.user && (
