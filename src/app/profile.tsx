d3a2682d96da87037689d972fdb3d99edcd726fd
Diego <dpadula@epe.santafe.gov.ar>
2025-11-28 13:39:03 -0300

    Obteniendo la sesion de supa en un useeffect

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { Session } from '@supabase/supabase-js';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { supabase } from '../lib/supabase';
 
 const ProfileScreen = () => {
+  const [session, setSession] = useState<Session | null>(null);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
   return (
     <View>
-      <Text>ProfileScreen</Text>
+      {session && session.user && (
+        <Text>
+          {session.user.id} {session.user.email}
+        </Text>
+      )}
     </View>
   );
 };
