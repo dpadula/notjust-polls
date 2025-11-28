7498bb60a96a3dea326dc6b8d8837e0a84299999
Diego <dpadula@epe.santafe.gov.ar>
2025-11-28 14:16:43 -0300

    El AuthLayout hace lo contrario, si hay usuario redirige a las rutas protegidas

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Redirect, Slot } from 'expo-router';
+import React from 'react';
+import { useAuth } from '../providers/AuthProvider';
+
+const AuthLayout = () => {
+  const { user } = useAuth();
+
+  // Este layout protege las rutas hijas, redirigiendo a login si hay usuario
+  // y sino renderiza el Slot (contenido hijo)
+  if (user) return <Redirect href='/profile' />;
+  return <Slot />;
+};
+
+export default AuthLayout;
